d252f8e262c0d818730bfb330065d33a63968127
ssteggell <spencer.steggell@gmail.com>
2024-11-06 13:38:56 -0800

    Initial Commit

---
+++ iFlexTests/iFlexTests.swift
@@ -0,0 +1,17 @@
+//
+//  iFlexTests.swift
+//  iFlexTests
+//
+//  Created by Spencer Steggell on 11/6/24.
+//
+
+import Testing
+@testable import iFlex
+
+struct iFlexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
